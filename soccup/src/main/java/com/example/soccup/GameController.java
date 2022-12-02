ed0425fb38bb4748733d0696136284e2c6b7ec01
oyoungsun <plusmate@naver.com>
2022-12-03 01:11:19 +0900

    feat : playerShuffle

--- soccup/src/main/java/com/example/soccup/GameController.java
+++ soccup/src/main/java/com/example/soccup/GameController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -11,7 +12,8 @@ import java.util.List;
 public class GameController {
 
         @GetMapping("game/")
-        public List<Integer> player(){
-            return null;
+        public List<Integer> player(List<Integer> playerList){
+            Collections.shuffle(playerList);
+            return playerList;
         }
 }
