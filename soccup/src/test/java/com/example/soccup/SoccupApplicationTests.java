c83270654894603df3d69f17f4e67e6ae375614c
oyoungsun <plusmate@naver.com>
2022-12-02 22:25:30 +0900

    spring init

---
+++ soccup/src/test/java/com/example/soccup/SoccupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soccup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
